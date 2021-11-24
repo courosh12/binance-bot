3593571f6708b03d0b9afc355548343a30c0683b
Poorya <poorjamajidi@gmail.com>
2021-11-24 02:41:56 +0100

    add migrations

---
+++ Binance.Bot/Binance.Bot/Data/Migrations/20211124012514_ChangeInPrice.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Binance.Bot.Data.Migrations
+{
+    public partial class ChangeInPrice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ChangeInPrice",
+                table: "Bots",
+                newName: "ChangeInPriceUp");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ChangeInPriceDown",
+                table: "Bots",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChangeInPriceDown",
+                table: "Bots");
+
+            migrationBuilder.RenameColumn(
+                name: "ChangeInPriceUp",
+                table: "Bots",
+                newName: "ChangeInPrice");
+        }
+    }
+}
